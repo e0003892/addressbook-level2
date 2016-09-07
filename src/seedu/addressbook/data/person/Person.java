97aaa878ed15d09e221dc2632ea0de90f1326961
e0003892 <e0003892.u.nus.edu>
2016-09-07 16:25:26 +0800

    Add an instance-level member and a class-level variable to the Person class

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -14,6 +14,8 @@ public class Person implements ReadOnlyPerson {
     private Phone phone;
     private Email email;
     private Address address;
+    private int sequenceNumber;
+    private static int nextSequenceNumber = 1;
 
     private final UniqueTagList tags;
     /**
@@ -24,7 +26,10 @@ public class Person implements ReadOnlyPerson {
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.sequenceNumber = nextSequenceNumber;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        
+        nextSequenceNumber++;
     }
 
     /**
